521c85b763f0a8e06023e56f73e0f45a632cf30f
gmunnich <gabriela.munnich@gmail.com>
2015-08-05 14:26:41 +0200

    lade till init

--- robotsstore.py
+++ robotsstore.py
@@ -1,38 +1,22 @@
 class Product(object):
- 
-  price = 0 
-  count = 0
-  tax = 0
+    def __init__(self,price,count,tax):
 
-  def price_with_tax(self):
-  	return self.price * self.count * self.tax 
-
-robot=Product()
-robot.price = 900
-robot.count = 2
-robot.tax = 1.25
-
-book=Product()
-book.price = 100
-book.count = 1
-book.tax  = 1.06
-
-print robot.price_with_tax()+book.price_with_tax()
-
-
-		
+         self.price = price 
+         self.count = count
+         self.tax = tax
 
 
 
+    def price_with_tax(self):
+        return self.price *self.count * self.tax 
+  
+
+robot=Product(price=900,count=2,tax=1.25)
+
+book=Product(price=100,count=1,tax=1.06)
 
 
-
- 	
-
-
-
-print robot.price*robot.count*robot.tax+book.price*book.count*book.tax
-
+print robot.price_with_tax() + book.price_with_tax()
 
  	
 
