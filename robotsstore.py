5301c555c8b5af375e7dfdcde8b77453d8f94e39
gmunnich <gabriela.munnich@gmail.com>
2015-08-05 14:48:21 +0200

    gjort en lista

--- robotsstore.py
+++ robotsstore.py
@@ -11,12 +11,11 @@ class Product(object):
         return self.price *self.count * self.tax 
   
 
-robot=Product(price=900,count=2,tax=1.25)
+products = [Product(price=900,count=2,tax=1.25),Product(price=100,count=1,tax=1.06)]
+total_price = products[0].price_with_tax() + products[1].price_with_tax()
 
-book=Product(price=100,count=1,tax=1.06)
 
-
-print robot.price_with_tax() + book.price_with_tax()
+print total_price
 
  	
 
